cc3fabaf75f400d6bfe1e32e454951a2345f9bb2
Севернюк Дмитрий <SeverniukDm@AREON.LOCAL>
2018-08-06 15:25:36 +0300

    added initial Form component structure

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+//custom
+//import bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
