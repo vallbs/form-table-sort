b8358372abb73aabb26a13c77950aba0ee6e9eb4
Севернюк Дмитрий <SeverniukDm@AREON.LOCAL>
2018-08-06 15:48:57 +0300

    added initial styling for Form component

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+    handleInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
     render() {
         return (
-            <div>Form</div>
+            <div className="container">
+                <form className="personForm">
+                    <div className="form-group row">
+                        <label htmlFor="firstName" className="col-form-label col-2 col-sm-2">First Name: </label>
+                        <div className="col-sm-4">
+                            <input 
+                                type="text" 
+                                className="form-control col-5 col-sm-5" 
+                                name="firstName"
+                                value={ this.state.firstName }
+                                onChange={ e => this.handleInputChange(e)} />
+                        </div>
+                        <div className="col-5 col-sm-5">
+                        <small id="passwordHelpInline" className="text-muted">
+                            Must be 8-20 characters long.
+                        </small>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="lastName" className="col-form-label col-2 col-sm-2">Last Name: </label>
+                        <div className="col-sm-4">
+                            <input 
+                                type="text" 
+                                className="form-control col-5 col-sm-5" 
+                                name="lastName"
+                                value={ this.state.lastName }
+                                onChange={ e => this.handleInputChange(e)} />
+                        </div>
+                        <div className="col-5 col-sm-5">
+                        <small id="passwordHelpInline" className="text-muted">
+                            Must be 8-20 characters long.
+                        </small>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary">Create Person</button>
+                </form>
+            </div>
         );
+    }
+
+    state = {
+        firstName: "",
+        lastName: "",
+        gender: "",
+        age: 0
     }
 }
 
