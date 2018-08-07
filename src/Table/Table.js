6d9edd6dc5fff59b8bed64980c0d70e158dc8291
Севернюк Дмитрий <SeverniukDm@AREON.LOCAL>
2018-08-07 16:01:59 +0300

    continue working on sort functionality

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -2,23 +2,62 @@ import React, { Component } from 'react';
 import './Table.css';
 
 class Table extends Component {
+    componentDidMount = () => {
+        //this.handleSortData(this.state.contacts, "firstName")
+    }
+
     handleDeleteContact = (contactId) => {
         console.log(contactId);
     }
 
     handleSortData = (data, sortField) => {
-        console.log("data", data);
+        console.log("sortField", sortField);
 
         let sortedData = [...data];
+        let sortDirectionAsc = this.state.sortDirection[sortField] === "asc";
         sortedData.sort( (a, b) => {
-            return a.firstName.localeCompare(b.firstName);
+            switch(sortField) {
+                case "firstName":
+                case "lastName":
+                case "phone":
+                    return 
+                    sortDirectionAsc ? 
+                        b[sortField].localeCompare(a[sortField]) :
+                        a[sortField].localeCompare(b[sortField]);
+                    break;
+                case "age":
+                    return 
+                        sortDirectionAsc ?
+                        b[sortField] - a[sortField] :
+                        a[sortField] - b[sortField];
+                    break;
+                case "gender":
+                    return 
+                        sortDirectionAsc ?
+                        ( a[sortField] == b[sortField] ? 0 : a[sortField] ? -1 : 1 ) :
+                        a[sortField] == b[sortField] ? 0 : a[sortField] ? 1 : -1;
+                    break;
+                default:
+                    break;
+                
+            }
+            return a[sortField].localeCompare(b[sortField]);
         });
 
-        //console.log("sortedData", sortedData);
-        this.setState({ contacts: sortedData });
+        //sortedData = sortedData.reverse();
+        
+        this.setState({ 
+            contacts: sortedData,
+            sortDirection: { 
+                ...this.state.sortDirection, 
+                [sortField]: this.state.sortDirection[sortField] === "asc" ? "desc" : "asc"
+            }
+        });
     }
 
     render() {
+        console.log(this.state);
+
         let contacts = <p>loading</p>
         if(this.state.contacts) {
             contacts = this.state.contacts.map(contact => {
@@ -43,10 +82,14 @@ class Table extends Component {
                 <thead>
                     <tr>
                         <th onClick={ (data, sortField) => this.handleSortData(this.state.contacts, "firstName") }>First Name <i className="fa fa-sort-up"></i></th>
-                        <th>Last Name</th>
+                        <th onClick={ (data, sortField) => this.handleSortData(this.state.contacts, "lastName") }>Last Name <i className="fa fa-sort-up"></i></th>
+                        <th onClick={ (data, sortField) => this.handleSortData(this.state.contacts, "phone") }>Phone <i className="fa fa-sort-up"></i></th>
+                        <th onClick={ (data, sortField) => this.handleSortData(this.state.contacts, "age") }>Age <i className="fa fa-sort-up"></i></th>
+                        <th onClick={ (data, sortField) => this.handleSortData(this.state.contacts, "gender") }>Gender <i className="fa fa-sort-up"></i></th>
+                        {/* <th>Last Name</th>
                         <th>Phone</th>
                         <th>Age</th>
-                        <th>Gender</th>
+                        <th>Gender</th> */}
                         <th></th>
                     </tr>
                 </thead>
@@ -65,7 +108,14 @@ class Table extends Component {
             {firstName: "Julia", lastName: "Voronich", phone: "096 123-45-67", age: 25, gender: false, id: "1"},
             {firstName: "Kolia", lastName: "Dombrovskii", phone: "093 657-23-46", age: 26, gender: true, id: "4"},
             {firstName: "Ivan", lastName: "Voronich", phone: "096 123-45-67", age: 25, gender: false, id: "2"}
-        ]
+        ],
+        sortDirection: {
+            firstName: "asc",
+            lastName: null,
+            phone: null,
+            age: null,
+            gender: null
+        }
     }
 }
 
