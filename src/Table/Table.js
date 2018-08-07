0d0bbaa0141d9a5e4613f0a0cfbdaf6c026b36a7
Севернюк Дмитрий <SeverniukDm@AREON.LOCAL>
2018-08-07 15:23:43 +0300

    added initial sort functionality

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -6,19 +6,32 @@ class Table extends Component {
         console.log(contactId);
     }
 
+    handleSortData = (data, sortField) => {
+        console.log("data", data);
+
+        let sortedData = [...data];
+        sortedData.sort( (a, b) => {
+            return a.firstName.localeCompare(b.firstName);
+        });
+
+        //console.log("sortedData", sortedData);
+        this.setState({ contacts: sortedData });
+    }
+
     render() {
         let contacts = <p>loading</p>
         if(this.state.contacts) {
             contacts = this.state.contacts.map(contact => {
                 return (
-                    <tr key={ contact.phone }>
+                    <tr key={ contact.id }>
                         <td>{ contact.firstName }</td>
                         <td>{ contact.lastName }</td>
                         <td>{ contact.phone }</td>
                         <td>{ contact.age }</td>
                         <td>{ "" + contact.gender }</td>
                         <th >
-                            <button onClick={ contactId => this.handleDeleteContact(contact.id) }>delete</button>
+                            <i className="fa fa-trash"></i>
+                            {/* <button onClick={ contactId => this.handleDeleteContact(contact.id) }>delete</button> */}
                         </th>
                     </tr>
                 );
@@ -29,7 +42,7 @@ class Table extends Component {
                 <table className="table table-bordered table-hover table-sm table-responsive ">
                 <thead>
                     <tr>
-                        <th>First Name</th>
+                        <th onClick={ (data, sortField) => this.handleSortData(this.state.contacts, "firstName") }>First Name <i className="fa fa-sort-up"></i></th>
                         <th>Last Name</th>
                         <th>Phone</th>
                         <th>Age</th>
@@ -47,9 +60,11 @@ class Table extends Component {
 
     state = {
         contacts: [
-            {firstName: "Dima", lastName: "Severniuk", phone: "093 277-44-62", age: 27, gender: true, id: "1"},
-            {firstName: "Roma", lastName: "Dombrovskii", phone: "093 657-23-46", age: 26, gender: true, id: "2"},
-            {firstName: "Julia", lastName: "Voronich", phone: "096 123-45-67", age: 25, gender: false, id: "3"}
+            {firstName: "Dima", lastName: "Severniuk", phone: "093 277-44-62", age: 27, gender: true, id: "3"},
+            {firstName: "Roma", lastName: "Dombrovskii", phone: "093 657-23-46", age: 26, gender: true, id: "5"},
+            {firstName: "Julia", lastName: "Voronich", phone: "096 123-45-67", age: 25, gender: false, id: "1"},
+            {firstName: "Kolia", lastName: "Dombrovskii", phone: "093 657-23-46", age: 26, gender: true, id: "4"},
+            {firstName: "Ivan", lastName: "Voronich", phone: "096 123-45-67", age: 25, gender: false, id: "2"}
         ]
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 //custom
-//import bootstrap
-import 'bootstrap/dist/css/bootstrap.css';
+//import bootstrap styles
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+//import font awesome styles
+import 'font-awesome/css/font-awesome.min.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
